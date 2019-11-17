fa0e4c795a41a2bc785130f61a94c49c193834dd
Daniele Esposti <daniele.esposti@gmail.com>
2019-11-17 22:24:57 +0000

    f

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+use log::{debug, error, info, warn};
+
+const PREFIX: &str = "back-";
+
 fn main() {
-    println!("Hello, world!");
+    simple_logger::init().unwrap();
+
+    let matches = App::new("tm-backup")
+        .version("0.1.0")
+        .author("Daniele Esposti <daniele.esposti@gmail.com>")
+        .about("")
+        .arg(
+            Arg::with_name("SOURCE")
+                .help("Source path")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("DEST")
+                .help("Destination path")
+                .required(true)
+                .index(2),
+        )
+        .get_matches();
+
+    // Same as above examples...
 }
